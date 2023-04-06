0674abda90169f2008edc20f602ef5ef2c05c71f
Priscila <priscilasartori2@gmail.com>
2023-04-05 23:41:19 -0300

    feat: Implementando a função getById no controller de teams

--- app/backend/src/database/controllers/teamsController.ts
+++ app/backend/src/database/controllers/teamsController.ts
@@ -12,6 +12,16 @@ class TeamController {
       next(error);
     }
   };
+
+  public getById = async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    try {
+      const team = await this.service.getById(Number(id));
+      return res.status(200).json(team);
+    } catch (error) {
+      next(error);
+    }
+  };
 }
 
 export default TeamController;
