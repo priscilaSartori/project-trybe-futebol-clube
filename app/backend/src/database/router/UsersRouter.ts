eb1e1c7ddf9f728090bffb6b968c06a7890c5365
Priscila <priscilasartori2@gmail.com>
2023-04-09 21:27:15 -0300

    fix: Alteração da rota e da model

--- app/backend/src/database/router/UsersRouter.ts
+++ app/backend/src/database/router/UsersRouter.ts
@@ -2,11 +2,12 @@ import { Router } from 'express';
 import Service from '../services/usersService';
 import Controller from '../controllers/usersController';
 import Model from '../models/usersModel';
+import validateToken from '../middleware/validateToken';
 
 const usersService = new Service(Model);
 const usersController = new Controller(usersService);
 
 const usersRouter = Router();
-usersRouter.get('/', (req, res, next) => usersController.getAll(req, res, next));
+usersRouter.post('/', validateToken.validate, usersController.login);
 
 export default usersRouter;
