369ebbee809a2bf7901695cd607bfd253f1fe57a
Priscila <priscilasartori2@gmail.com>
2023-04-09 21:43:11 -0300

    feat: Implementando a validação do Token

---
+++ app/backend/src/database/middleware/validateToken.ts
@@ -0,0 +1,11 @@
+import { NextFunction, Request, Response } from 'express';
+
+export default class validateToken {
+  public static validate(req: Request, res: Response, next: NextFunction) {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: 'All fields must be filled' });
+    }
+    next();
+  }
+}
