6a9626de4b1afa9957e61b4d1a4f5b119846cba8
Priscila <priscilasartori2@gmail.com>
2023-04-12 11:34:35 -0300

    feat: Implementando a rota  /:id/finish de matches

--- app/backend/src/database/router/MatchesRouter.ts
+++ app/backend/src/database/router/MatchesRouter.ts
@@ -3,11 +3,13 @@ import Service from '../services/matchesService';
 import Controller from '../controllers/matchesController';
 import Model from '../models/matchesModel';
 import TeamsModel from '../models/teamsModel';
+import validateToken from '../middleware/validateToken';
 
 const matchesService = new Service(Model, TeamsModel);
 const matchesController = new Controller(matchesService);
 
 const matchesRouter = Router();
 matchesRouter.get('/', matchesController.getAll);
+matchesRouter.patch('/:id/finish', validateToken.validate, matchesController.update);
 
 export default matchesRouter;
