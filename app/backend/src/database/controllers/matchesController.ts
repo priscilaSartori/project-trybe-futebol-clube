49971bb66303ade999b84f26d67c1cb53f4be291
Priscila <priscilasartori2@gmail.com>
2023-04-12 00:39:38 -0300

    fix: alterando o controller  de matches

--- app/backend/src/database/controllers/matchesController.ts
+++ app/backend/src/database/controllers/matchesController.ts
@@ -5,21 +5,14 @@ class MatchesController {
   constructor(private matchesService: MatchesService) { }
 
   public getAll = async (req: Request, res: Response, next: NextFunction) => {
+    const { inProgress } = req.query;
+    if (inProgress) {
+      const matchesInProgress = await this.matchesService.getInProgress(inProgress as string);
+      res.status(200).json(matchesInProgress);
+    }
     try {
       const matches = await this.matchesService.getAll();
       res.status(200).json(matches);
-    } catch (error) {
-      next(error);
-    }
-  };
-
-  public getInProgress = async (req: Request, res: Response, next: NextFunction) => {
-    const { inProgress } = req.query;
-    try {
-      if (inProgress) {
-        const matchesInProgress = await this.matchesService.getInProgress(inProgress as string);
-        res.status(200).json(matchesInProgress);
-      }
     } catch (error) {
       next(error);
     }
