ecaafcf8f4398f0d5cff6a37be0fc796781b1af6
Priscila <priscilasartori2@gmail.com>
2023-04-12 00:22:44 -0300

    feat: Implementando o getInProgress no controller de matches

--- app/backend/src/database/controllers/matchesController.ts
+++ app/backend/src/database/controllers/matchesController.ts
@@ -4,10 +4,22 @@ import MatchesService from '../services/matchesService';
 class MatchesController {
   constructor(private matchesService: MatchesService) { }
 
-  public getAll = async (_req: Request, res: Response, next: NextFunction) => {
+  public getAll = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const matches = await this.matchesService.getAll();
       res.status(200).json(matches);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public getInProgress = async (req: Request, res: Response, next: NextFunction) => {
+    const { inProgress } = req.query;
+    try {
+      if (inProgress) {
+        const matchesInProgress = await this.matchesService.getInProgress(inProgress as string);
+        res.status(200).json(matchesInProgress);
+      }
     } catch (error) {
       next(error);
     }
